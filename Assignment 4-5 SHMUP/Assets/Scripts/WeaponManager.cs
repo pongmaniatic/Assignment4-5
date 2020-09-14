4addfe88410b32e4ab82ef70ff072501f607a0da
pongmaniatic <37563473+pongmaniatic@users.noreply.github.com>
2020-09-15 01:26:19 +0200

    little update

--- Assignment 4-5 SHMUP/Assets/Scripts/WeaponManager.cs
+++ Assignment 4-5 SHMUP/Assets/Scripts/WeaponManager.cs
@@ -9,7 +9,7 @@ public class Bullet1 : MonoBehaviour, IWeapon
         Vector3 initialPosition = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
         GameObject bullet = Instantiate(Resources.Load("Bullet1", typeof(GameObject))) as GameObject;
         bullet.transform.position = initialPosition;
-        bullet.GetComponent<Rigidbody2D>().velocity = transform.right * 10;
+        bullet.GetComponent<Rigidbody2D>().velocity = transform.right * 15;
     }
 }
 public class Bullet2 : MonoBehaviour, IWeapon
@@ -19,7 +19,7 @@ public class Bullet2 : MonoBehaviour, IWeapon
         Vector3 initialPosition = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
         GameObject bullet = Instantiate(Resources.Load("Bullet2", typeof(GameObject))) as GameObject;
         bullet.transform.position = initialPosition;
-        bullet.GetComponent<Rigidbody2D>().velocity = transform.right * 10;
+        bullet.GetComponent<Rigidbody2D>().velocity = transform.right * 15;
     }
 }
 
